79c0a4b63aed6f485a9c62fd8f2f20bb7309b91b
Brandon Diaz <Brandon Diaz>
2017-04-23 09:22:06 -0400

    This might work...

---
+++ docs/dataFile.js
@@ -0,0 +1,49 @@
+var dialogueScript =
+{
+	"hackru": [
+		{
+			"questions":
+			[
+				{
+					"questionType":"number",
+					"questionText":"On a scale of 1 to 10, how would you rate your overall experience at hack r u?"
+				},
+				{
+					"questionType":"number",
+					"questionText":"On a scale of 1 to 10, how would you rate your experience waiting in a line for food or tee shirts?"
+				},
+				{
+					"questionType":"number",
+					"questionText":"On a scale of 1 to 10, how would you rate the awesomeness of the sponsoring companies?"
+				},
+				{
+					"questionType":"number",
+					"questionText":"On a scale of 1 to 10, how would you rate the meals, drinks, and snacks?"
+				},
+				{
+					"questionType":"open",
+					"questionText":"What was the best thing about hack r u?"
+				},
+				{
+					"questionType":"open",
+					"questionText":"What was the worst thing about hack r u?"
+				},
+				{
+					"questionType":"open",
+					"questionText":"Tell me any last comments you have about hack r u."
+				}
+			]
+		},
+		{
+			"story_id": "0",
+			"business": "hack r u",
+			"welcome_message": "Thank you for coming to hack r u, number 12. I truly hope you learned something new today."
+		}
+	],
+	"barber": [
+		{
+			"story_id": "1",
+			"business": "Alan's barber shop"
+		}
+	]
+};
